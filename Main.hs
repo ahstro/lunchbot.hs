66edb07015e0043803962c2608e0583475a2f2c3
Anton Strömkvist <hi@ahst.ro>
2017-06-14 11:05:50 +0200

    Extract menu using TagSoup

--- Main.hs
+++ Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
+import Data.Char (isAlpha, toLower)
 import Data.Time (utctDay)
 import Data.Time.Calendar.WeekDate (toWeekDate)
 import Data.Time.Clock (getCurrentTime)
-
 import Network.HTTP (getRequest, getResponseBody, simpleHTTP)
+import Text.HTML.TagSoup
 
 cremeURL :: String
 cremeURL = "http://bistro.creme.se/meny"
@@ -17,4 +18,22 @@ main = do
   src <- openURL cremeURL
   time <- getCurrentTime
   let (_, _, weekday) = toWeekDate $ utctDay time
-  putStrLn $ show weekday
+  let tags = parseTags src
+  putStrLn $ getMenu "1316" tags
+  putStrLn $ getMenu "1317" tags
+  putStrLn $ getMenu "1353" tags
+  putStrLn $ getMenu "1354" tags
+  putStrLn $ getMenu "1355" tags
+  where
+    getMenu id =
+      unlines .
+      filter ((> 5) . length . words) .
+      map capitalize .
+      map unwords .
+      filter ((> 0) . length) .
+      map words .
+      lines .
+      innerText .
+      takeWhile (~/= "<div class=media-themed>") .
+      dropWhile (~/= ("<div id=category-" ++ id ++ "-courses>"))
+    capitalize (head:tail) = head : map toLower tail
